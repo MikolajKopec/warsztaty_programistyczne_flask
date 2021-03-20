20856d49eae88c4f63ae6beec7e5fd9bd2ccabdc
Khapen <mikolajkopec772@gmail.com>
2021-03-20 11:21:29 +0100

    complete task 3

--- flask_01/app.py
+++ flask_01/app.py
@@ -1,10 +1,24 @@
-from flask import Flask, render_template,request
+from flask import Flask, render_template,request,redirect
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/',methods=('GET','POST',))
 def index():
     return render_template('index.html')
-@app.route('/example', methods = ('GET','POST',))
+@app.route('/login', methods=('GET','POST'))
+def login():
+    passw=""
+    log = request.form['login']
+    passw = request.form['password']
+    success="Zostałeś zalogowany!"
+    error = "Błędne hasło!"
+    if passw!="jeleń":
+        return render_template('index.html',error = error,login = log)
+    elif passw=="jeleń":
+        return redirect('calc')
+@app.route('/calc', methods = ('GET','POST',))
+def calc():
+    return render_template('calc.html')
+@app.route('/calc_value', methods = ('GET','POST',))
 def example():
     a = float(request.form['a'])
     b = float(request.form['b'])
@@ -17,6 +31,6 @@ def example():
         wynik = a*b
     elif s=="/":
         wynik = a/b
-    return str(wynik)
+    return render_template('calc.html', wynik = wynik)
 if __name__ == '__main__':
     app.run(debug=True)
