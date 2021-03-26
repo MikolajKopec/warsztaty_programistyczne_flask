703d841d2f30215bb3ba034a389569efaf0e03c5
Khapen <mikolajkopec772@gmail.com>
2021-03-26 13:02:01 +0100

    Update task 3 - correct

--- flask_01/app.py
+++ flask_01/app.py
@@ -1,36 +1,35 @@
 from flask import Flask, render_template,request,redirect
 app = Flask(__name__)
 
-@app.route('/',methods=('GET','POST',))
-def index():
-    return render_template('index.html')
 @app.route('/login', methods=('GET','POST'))
 def login():
-    passw=""
-    log = request.form['login']
-    passw = request.form['password']
-    success="Zostałeś zalogowany!"
-    error = "Błędne hasło!"
-    if passw!="jeleń":
-        return render_template('index.html',error = error,login = log)
-    elif passw=="jeleń":
-        return redirect('calc')
+    if request.method =='GET':
+        return render_template('index.html')
+    elif request.method =='POST':
+        log = request.form['login']
+        passw = request.form['password']
+        success="Zostałeś zalogowany!"
+        error = "Błędne hasło!"
+        if passw!="jeleń":
+            return render_template('index.html',error = error,login = log)
+        elif passw=="jeleń":
+            return redirect('calc')
 @app.route('/calc', methods = ('GET','POST',))
-def calc():
-    return render_template('calc.html')
-@app.route('/calc_value', methods = ('GET','POST',))
 def example():
-    a = float(request.form['a'])
-    b = float(request.form['b'])
-    s = request.form['sign']
-    if s=="+":
-        wynik = a+b
-    elif s=="-":
-        wynik = a-b
-    elif s=="*":
-        wynik = a*b
-    elif s=="/":
-        wynik = a/b
-    return render_template('calc.html', wynik = wynik)
+    if request.method=='GET':
+            return render_template('calc.html')
+    elif request.method=='POST':
+        a = float(request.form['a'])
+        b = float(request.form['b'])
+        s = request.form['sign']
+        if s=="+":
+            wynik = a+b
+        elif s=="-":
+            wynik = a-b
+        elif s=="*":
+            wynik = a*b
+        elif s=="/":
+            wynik = a/b
+        return render_template('calc.html', wynik = wynik)
 if __name__ == '__main__':
     app.run(debug=True)
