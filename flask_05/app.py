17db2ab427b112a7fa5e6903f3eac40576e4e7ea
Khapen <mikolajkopec772@gmail.com>
2021-06-08 22:32:53 +0200

    improve creating new auctions, add view existing autions, add posibility to bid on a auction

---
+++ flask_05/app.py
@@ -0,0 +1,10 @@
+from flask import Flask, render_template,request,redirect
+from flask_sqlalchemy import SQLAlchemy
+app = Flask(__name__)
+
+db = SQLAlchemy(app)
+@app.route('/')
+def index():
+    return render_template('index.html')
+if __name__ == '__main__':
+    app.run(debug=True)
